e6196113fa3042c8c8cd9e5eec3df61087f7c892
Christoffer Niska <christofferniska@gmail.com>
2016-02-28 21:59:30 +0200

    Update distribution build

--- dist/shared/helpers/game.js
+++ dist/shared/helpers/game.js
@@ -3,5 +3,5 @@
  * @param {Array} entities
  * @param {string} id
  * @returns {number}
- */function findEntityIndexById(entities,id){var result=-1;for(var i=0;i<entities.length;i++){if(entities[i].id===id){result=i}}return result}
+ */function findEntityIndexById(entities,id){var result=-1;for(var i=0;i<entities.length;i++){if(entities[i].id&&entities[i].id===id){result=i}}return result}
 //# sourceMappingURL=game.js.map
